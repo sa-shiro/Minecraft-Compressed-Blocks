02ed3a1ddc40e9070724a6f48e7bc3f258a9cdc1
Sashiro <social.sashiro@outlook.com>
2020-01-24 22:11:48 +0100

    removed static CompressedBlocksInit c

--- src/main/java/com/github/sa_shiro/compressedblocks/Events/RegistryEvents.java
+++ src/main/java/com/github/sa_shiro/compressedblocks/Events/RegistryEvents.java
@@ -1,7 +1,7 @@
 package com.github.sa_shiro.compressedblocks.Events;
 
-import com.github.sa_shiro.compressedblocks.blocks.CompressedBlocksInit;
 import com.github.sa_shiro.compressedblocks.blocks.CompressedBlock;
+import com.github.sa_shiro.compressedblocks.blocks.CompressedBlocksInit;
 import com.github.sa_shiro.compressedblocks.init.Init;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
@@ -12,15 +12,16 @@ import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+import java.util.Objects;
+
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class RegistryEvents {
-    private static CompressedBlocksInit c;
     @SubscribeEvent
     public static void registerCompressedBlocks(final RegistryEvent.Register<Block> e) {
         e.getRegistry().registerAll(
-                c.DOUBLE_COMPRESSED_STONE = new CompressedBlock(Material.ROCK, SoundType.STONE, 0,"stone", "Stone", 2.0f, 50.0f, 1),
-                c.DOUBLE_COMPRESSED_GRANITE = new CompressedBlock(Material.ROCK, SoundType.STONE, 0,"granite", "Granite", 2.0f, 50.0f, 1),
-                c.DOUBLE_COMPRESSED_DIORITE = new CompressedBlock(Material.ROCK, SoundType.STONE, 0,"diorite", "Diorite", 2.0f, 50.0f, 1)
+                CompressedBlocksInit.DOUBLE_COMPRESSED_STONE = new CompressedBlock(Material.ROCK, SoundType.STONE, 0, "stone", "Stone", 2.0f, 50.0f, 1),
+                CompressedBlocksInit.DOUBLE_COMPRESSED_GRANITE = new CompressedBlock(Material.ROCK, SoundType.STONE, 0, "granite", "Granite", 2.0f, 50.0f, 1),
+                CompressedBlocksInit.DOUBLE_COMPRESSED_DIORITE = new CompressedBlock(Material.ROCK, SoundType.STONE, 0, "diorite", "Diorite", 2.0f, 50.0f, 1)
         );
     }
 
@@ -29,9 +30,9 @@ public class RegistryEvents {
         Item.Properties props = new Item.Properties()
                 .group(Init.doubleCompressedGroup);
         e.getRegistry().registerAll(
-                new BlockItem(c.DOUBLE_COMPRESSED_STONE, props).setRegistryName(c.DOUBLE_COMPRESSED_STONE.getRegistryName()),
-                new BlockItem(c.DOUBLE_COMPRESSED_GRANITE, props).setRegistryName(c.DOUBLE_COMPRESSED_GRANITE.getRegistryName()),
-                new BlockItem(c.DOUBLE_COMPRESSED_DIORITE, props).setRegistryName(c.DOUBLE_COMPRESSED_DIORITE.getRegistryName())
+                new BlockItem(CompressedBlocksInit.DOUBLE_COMPRESSED_STONE, props).setRegistryName(Objects.requireNonNull(CompressedBlocksInit.DOUBLE_COMPRESSED_STONE.getRegistryName())),
+                new BlockItem(CompressedBlocksInit.DOUBLE_COMPRESSED_GRANITE, props).setRegistryName(Objects.requireNonNull(CompressedBlocksInit.DOUBLE_COMPRESSED_GRANITE.getRegistryName())),
+                new BlockItem(CompressedBlocksInit.DOUBLE_COMPRESSED_DIORITE, props).setRegistryName(Objects.requireNonNull(CompressedBlocksInit.DOUBLE_COMPRESSED_DIORITE.getRegistryName()))
         );
     }
 }
