181a9ec8d0fe95925f33802edb365df786448fa6
sa-shiro <snygglove_mira@outlook.com>
2020-04-19 16:41:10 +0200

    added accesstransformer.cfg transforming RenderType.TRANSCULENT > public
    Signed-off-by: sa-shiro <snygglove_mira@outlook.com>

--- src/main/java/com/github/sa_shiro/compressedblocks/init/Init.java
+++ src/main/java/com/github/sa_shiro/compressedblocks/init/Init.java
@@ -7,7 +7,7 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 
 public class Init {
-    private static final RenderType rendertype = RenderType.getTranslucent();
+    private static final RenderType TRANSLUCENT = RenderType.TRANSLUCENT;
     public static ItemGroup compressedGroup = new ItemGroup("compressed_blocks") {
         @Override
         public ItemStack createIcon() {
@@ -21,89 +21,89 @@ public class Init {
     };
 
     public void init() {
-        RenderTypeLookup.setRenderLayer(Blocks.Single.COMPRESSED_ANDESITE, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Single.COMPRESSED_COBBLESTONE, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Single.COMPRESSED_DIORITE, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Single.COMPRESSED_DIRT, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Single.COMPRESSED_GRANITE, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Single.COMPRESSED_GRAVEL, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Single.COMPRESSED_NETHERRACK, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Single.COMPRESSED_RED_SAND, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Single.COMPRESSED_REDSTONE_BLOCK, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Single.COMPRESSED_SAND, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Single.COMPRESSED_SOUL_SAND, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Single.COMPRESSED_STONE, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Single.COMPRESSED_DIAMOND_BLOCK, rendertype);
+        RenderTypeLookup.setRenderLayer(Blocks.Single.COMPRESSED_ANDESITE, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Single.COMPRESSED_COBBLESTONE, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Single.COMPRESSED_DIORITE, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Single.COMPRESSED_DIRT, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Single.COMPRESSED_GRANITE, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Single.COMPRESSED_GRAVEL, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Single.COMPRESSED_NETHERRACK, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Single.COMPRESSED_RED_SAND, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Single.COMPRESSED_REDSTONE_BLOCK, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Single.COMPRESSED_SAND, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Single.COMPRESSED_SOUL_SAND, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Single.COMPRESSED_STONE, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Single.COMPRESSED_DIAMOND_BLOCK, TRANSLUCENT);
 
-        RenderTypeLookup.setRenderLayer(Blocks.Double.DOUBLE_COMPRESSED_ANDESITE, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Double.DOUBLE_COMPRESSED_COBBLESTONE, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Double.DOUBLE_COMPRESSED_DIORITE, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Double.DOUBLE_COMPRESSED_DIRT, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Double.DOUBLE_COMPRESSED_GRANITE, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Double.DOUBLE_COMPRESSED_GRAVEL, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Double.DOUBLE_COMPRESSED_NETHERRACK, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Double.DOUBLE_COMPRESSED_RED_SAND, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Double.DOUBLE_COMPRESSED_REDSTONE_BLOCK, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Double.DOUBLE_COMPRESSED_SAND, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Double.DOUBLE_COMPRESSED_SOUL_SAND, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Double.DOUBLE_COMPRESSED_STONE, rendertype);
+        RenderTypeLookup.setRenderLayer(Blocks.Double.DOUBLE_COMPRESSED_ANDESITE, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Double.DOUBLE_COMPRESSED_COBBLESTONE, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Double.DOUBLE_COMPRESSED_DIORITE, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Double.DOUBLE_COMPRESSED_DIRT, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Double.DOUBLE_COMPRESSED_GRANITE, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Double.DOUBLE_COMPRESSED_GRAVEL, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Double.DOUBLE_COMPRESSED_NETHERRACK, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Double.DOUBLE_COMPRESSED_RED_SAND, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Double.DOUBLE_COMPRESSED_REDSTONE_BLOCK, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Double.DOUBLE_COMPRESSED_SAND, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Double.DOUBLE_COMPRESSED_SOUL_SAND, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Double.DOUBLE_COMPRESSED_STONE, TRANSLUCENT);
 
-        RenderTypeLookup.setRenderLayer(Blocks.Triple.TRIPLE_COMPRESSED_ANDESITE, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Triple.TRIPLE_COMPRESSED_COBBLESTONE, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Triple.TRIPLE_COMPRESSED_DIORITE, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Triple.TRIPLE_COMPRESSED_DIRT, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Triple.TRIPLE_COMPRESSED_GRANITE, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Triple.TRIPLE_COMPRESSED_GRAVEL, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Triple.TRIPLE_COMPRESSED_NETHERRACK, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Triple.TRIPLE_COMPRESSED_RED_SAND, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Triple.TRIPLE_COMPRESSED_REDSTONE_BLOCK, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Triple.TRIPLE_COMPRESSED_SAND, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Triple.TRIPLE_COMPRESSED_SOUL_SAND, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Triple.TRIPLE_COMPRESSED_STONE, rendertype);
+        RenderTypeLookup.setRenderLayer(Blocks.Triple.TRIPLE_COMPRESSED_ANDESITE, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Triple.TRIPLE_COMPRESSED_COBBLESTONE, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Triple.TRIPLE_COMPRESSED_DIORITE, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Triple.TRIPLE_COMPRESSED_DIRT, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Triple.TRIPLE_COMPRESSED_GRANITE, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Triple.TRIPLE_COMPRESSED_GRAVEL, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Triple.TRIPLE_COMPRESSED_NETHERRACK, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Triple.TRIPLE_COMPRESSED_RED_SAND, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Triple.TRIPLE_COMPRESSED_REDSTONE_BLOCK, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Triple.TRIPLE_COMPRESSED_SAND, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Triple.TRIPLE_COMPRESSED_SOUL_SAND, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Triple.TRIPLE_COMPRESSED_STONE, TRANSLUCENT);
 
-        RenderTypeLookup.setRenderLayer(Blocks.Quad.QUADRUPLE_COMPRESSED_ANDESITE, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Quad.QUADRUPLE_COMPRESSED_COBBLESTONE, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Quad.QUADRUPLE_COMPRESSED_DIORITE, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Quad.QUADRUPLE_COMPRESSED_DIRT, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Quad.QUADRUPLE_COMPRESSED_GRANITE, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Quad.QUADRUPLE_COMPRESSED_NETHERRACK, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Quad.QUADRUPLE_COMPRESSED_RED_SAND, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Quad.QUADRUPLE_COMPRESSED_SAND, rendertype);
+        RenderTypeLookup.setRenderLayer(Blocks.Quad.QUADRUPLE_COMPRESSED_ANDESITE, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Quad.QUADRUPLE_COMPRESSED_COBBLESTONE, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Quad.QUADRUPLE_COMPRESSED_DIORITE, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Quad.QUADRUPLE_COMPRESSED_DIRT, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Quad.QUADRUPLE_COMPRESSED_GRANITE, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Quad.QUADRUPLE_COMPRESSED_NETHERRACK, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Quad.QUADRUPLE_COMPRESSED_RED_SAND, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Quad.QUADRUPLE_COMPRESSED_SAND, TRANSLUCENT);
 
-        RenderTypeLookup.setRenderLayer(Blocks.Quin.QUINTUPLE_COMPRESSED_ANDESITE, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Quin.QUINTUPLE_COMPRESSED_COBBLESTONE, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Quin.QUINTUPLE_COMPRESSED_DIORITE, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Quin.QUINTUPLE_COMPRESSED_DIRT, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Quin.QUINTUPLE_COMPRESSED_GRANITE, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Quin.QUINTUPLE_COMPRESSED_NETHERRACK, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Quin.QUINTUPLE_COMPRESSED_SAND, rendertype);
+        RenderTypeLookup.setRenderLayer(Blocks.Quin.QUINTUPLE_COMPRESSED_ANDESITE, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Quin.QUINTUPLE_COMPRESSED_COBBLESTONE, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Quin.QUINTUPLE_COMPRESSED_DIORITE, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Quin.QUINTUPLE_COMPRESSED_DIRT, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Quin.QUINTUPLE_COMPRESSED_GRANITE, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Quin.QUINTUPLE_COMPRESSED_NETHERRACK, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Quin.QUINTUPLE_COMPRESSED_SAND, TRANSLUCENT);
 
-        RenderTypeLookup.setRenderLayer(Blocks.Sext.SEXTUPLE_COMPRESSED_ANDESITE, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Sext.SEXTUPLE_COMPRESSED_COBBLESTONE, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Sext.SEXTUPLE_COMPRESSED_DIORITE, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Sext.SEXTUPLE_COMPRESSED_DIRT, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Sext.SEXTUPLE_COMPRESSED_GRANITE, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Sext.SEXTUPLE_COMPRESSED_NETHERRACK, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Sext.SEXTUPLE_COMPRESSED_SAND, rendertype);
+        RenderTypeLookup.setRenderLayer(Blocks.Sext.SEXTUPLE_COMPRESSED_ANDESITE, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Sext.SEXTUPLE_COMPRESSED_COBBLESTONE, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Sext.SEXTUPLE_COMPRESSED_DIORITE, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Sext.SEXTUPLE_COMPRESSED_DIRT, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Sext.SEXTUPLE_COMPRESSED_GRANITE, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Sext.SEXTUPLE_COMPRESSED_NETHERRACK, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Sext.SEXTUPLE_COMPRESSED_SAND, TRANSLUCENT);
 
-        RenderTypeLookup.setRenderLayer(Blocks.Sept.SEPTUPLE_COMPRESSED_COBBLESTONE, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Sept.SEPTUPLE_COMPRESSED_DIRT, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Sept.SEPTUPLE_COMPRESSED_NETHERRACK, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Sept.SEPTUPLE_COMPRESSED_SAND, rendertype);
+        RenderTypeLookup.setRenderLayer(Blocks.Sept.SEPTUPLE_COMPRESSED_COBBLESTONE, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Sept.SEPTUPLE_COMPRESSED_DIRT, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Sept.SEPTUPLE_COMPRESSED_NETHERRACK, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Sept.SEPTUPLE_COMPRESSED_SAND, TRANSLUCENT);
 
-        RenderTypeLookup.setRenderLayer(Blocks.Oct.OCTUPLE_COMPRESSED_COBBLESTONE, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Oct.OCTUPLE_COMPRESSED_DIRT, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Oct.OCTUPLE_COMPRESSED_NETHERRACK, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Oct.OCTUPLE_COMPRESSED_SAND, rendertype);
+        RenderTypeLookup.setRenderLayer(Blocks.Oct.OCTUPLE_COMPRESSED_COBBLESTONE, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Oct.OCTUPLE_COMPRESSED_DIRT, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Oct.OCTUPLE_COMPRESSED_NETHERRACK, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Oct.OCTUPLE_COMPRESSED_SAND, TRANSLUCENT);
 
-        RenderTypeLookup.setRenderLayer(Blocks.Mega.MEGA_COMPRESSED_COBBLESTONE, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Mega.MEGA_COMPRESSED_DIRT, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Mega.MEGA_COMPRESSED_NETHERRACK, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Mega.MEGA_COMPRESSED_SAND, rendertype);
+        RenderTypeLookup.setRenderLayer(Blocks.Mega.MEGA_COMPRESSED_COBBLESTONE, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Mega.MEGA_COMPRESSED_DIRT, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Mega.MEGA_COMPRESSED_NETHERRACK, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Mega.MEGA_COMPRESSED_SAND, TRANSLUCENT);
 
-        RenderTypeLookup.setRenderLayer(Blocks.Giga.GIGA_COMPRESSED_COBBLESTONE, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Giga.GIGA_COMPRESSED_DIRT, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Giga.GIGA_COMPRESSED_NETHERRACK, rendertype);
-        RenderTypeLookup.setRenderLayer(Blocks.Giga.GIGA_COMPRESSED_SAND, rendertype);
+        RenderTypeLookup.setRenderLayer(Blocks.Giga.GIGA_COMPRESSED_COBBLESTONE, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Giga.GIGA_COMPRESSED_DIRT, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Giga.GIGA_COMPRESSED_NETHERRACK, TRANSLUCENT);
+        RenderTypeLookup.setRenderLayer(Blocks.Giga.GIGA_COMPRESSED_SAND, TRANSLUCENT);
     }
 }
