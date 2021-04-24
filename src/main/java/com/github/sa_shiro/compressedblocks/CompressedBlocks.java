6f39a3d587984d1a25a74501ac2297375b0ea990
sashiro <social.sashiro@outlook.com>
2021-04-24 21:24:40 +0200

    rewrote config for general reasons stone and cobblestone can not be disabled and their compression level can not be controlled (stone with compression level 9 is used as CreativeTab icon)
    Signed-off-by: sashiro <social.sashiro@outlook.com>

--- src/main/java/com/github/sa_shiro/compressedblocks/CompressedBlocks.java
+++ src/main/java/com/github/sa_shiro/compressedblocks/CompressedBlocks.java
@@ -1,8 +1,12 @@
 package com.github.sa_shiro.compressedblocks;
 
 import com.github.sa_shiro.compressedblocks.event.RegistryEvent;
+import com.github.sa_shiro.compressedblocks.util.ForgeConfigManager;
 import com.github.sa_shiro.compressedblocks.util.Lists;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.loading.FMLPaths;
 
 
 @Mod(CompressedBlocks.MOD_ID)
@@ -11,6 +15,16 @@ public class CompressedBlocks {
 
     public CompressedBlocks() {
         Lists.populate();
+
+        String config1 = "compressedblocks_compression.toml";
+        String config2 = "compressedblocks_blocks.toml";
+
+        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, ForgeConfigManager.configMaxCompressionLevel, config1);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, ForgeConfigManager.configEnabledBlocks, config2);
+
+        ForgeConfigManager.loadConfig(ForgeConfigManager.configMaxCompressionLevel, FMLPaths.CONFIGDIR.get().resolve(config1).toString());
+        ForgeConfigManager.loadConfig(ForgeConfigManager.configEnabledBlocks, FMLPaths.CONFIGDIR.get().resolve(config2).toString());
+
         RegistryEvent.register();
     }
 }
--- src/main/java/com/github/sa_shiro/compressedblocks/util/ItemGroups.java
+++ src/main/java/com/github/sa_shiro/compressedblocks/util/ItemGroups.java
@@ -1,14 +1,24 @@
 package com.github.sa_shiro.compressedblocks.util;
 
 import com.github.sa_shiro.compressedblocks.event.RegistryEvent;
+import net.minecraft.block.Block;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.RegistryObject;
+
+import java.util.Objects;
 
 public class ItemGroups {
     public static ItemGroup compressedBlockGroup = new ItemGroup("compressed_blocks") {
         @Override
         public ItemStack createIcon() {
-            return new ItemStack(RegistryEvent.BLOCK_REGISTRY.get(6).get());
+            Block icon = null;
+            for (RegistryObject<Block> block : RegistryEvent.BLOCK_REGISTRY) {
+                if (Objects.requireNonNull(block.get().getRegistryName()).toString().equals("compressedblocks:c9_stone")) {
+                    icon = block.get();
+                }
+            }
+            return new ItemStack(icon);
         }
 
         @Override
